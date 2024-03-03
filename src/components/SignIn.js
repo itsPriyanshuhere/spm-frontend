a29dfdc6ed8229d2e4eb745c0b4b0c8ce7c31b9c
itsPriyanshuhere <priyanshupandey256@gmail.com>
2024-03-03 15:23:13 +0530

    Intial Setup

---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+const SignIn = () => {
+    return (
+      <div className="flex h-screen">
+        <div className='bg-red w-1/2'></div>
+        {/* <div className='bg-green w-1/2'></div> */}
+      </div>
+    );
+  };
+  
+  export default SignIn;
+  
